bbfefacf086e6e357752806abacddaa6e19cbaac
Doddlin <didrik.birkemalm@gmail.com>
2018-11-19 21:39:24 +0100

    bad form

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,7 @@
 module EntriesHelper
+
+  def date_select(object_name, method, options = {}, html_options = {})
+        Tags::DateSelect.new(object_name, method, self, options, html_options).render
+      end
+      
 end

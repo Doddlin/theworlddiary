82fcf0d5a6bd9c8ea829f572083ec429673732b7
Doddlin <didrik.birkemalm@gmail.com>
2018-11-19 21:42:19 +0100

    new test

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,7 +1,2 @@
 module EntriesHelper
-
-  def date_select(object_name, method, options = {}, html_options = {})
-        Tags::DateSelect.new(object_name, method, self, options, html_options).render
-      end
-      
 end
